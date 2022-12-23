a999e1439017e65d6295ffdcc8b41bb750cad3fd
LegallyAlive <jarvisgan@gmail.com>
2022-12-23 09:28:05 +0800

    added companyID string to allow for company based viewing of reports

--- src/main/java/models/ReportModel.java
+++ src/main/java/models/ReportModel.java
@@ -10,7 +10,7 @@ public class ReportModel {
 
 	public Report find() {
 		return new Report("01", "11011", "11/11/20", ReportCategory.FOOD,
-				List.of(new InventoryItem("Oreo", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.FOOD)));
+				List.of(new InventoryItem("Oreo", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.FOOD)),"001");
 	}
 
 	/*
@@ -23,13 +23,14 @@ public class ReportModel {
 		List<Report> result = new ArrayList<Report>();
 		result.add(new Report("01", "110011", "11/11/20", ReportCategory.FOOD,
 				List.of(new InventoryItem("Oreo", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.FOOD),
-						new InventoryItem("cake", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.FOOD))));
+						new InventoryItem("cake", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.FOOD)),"001"));
 		result.add(new Report("02", "110022", "10/10/20", ReportCategory.DRINK,
 				List.of(new InventoryItem("Oreo", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.DRINK),
-						new InventoryItem("cake", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.DRINK))));
+						new InventoryItem("cake", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.DRINK)),"002"));
 		result.add(new Report("03", "110033", "09/09/20", ReportCategory.FOOD,
 				List.of(new InventoryItem("Oreo", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.FOOD),
-						new InventoryItem("cake", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.FOOD))));
+						new InventoryItem("cake", 1.0, 2.0, 3.0, 4.0, 5.0, ReportCategory.FOOD)),"003"));
+		
 		return result;
 	}
 
