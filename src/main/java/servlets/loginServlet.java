4267b6eb5bcbba259d28f65740395fdbbb365710
Victor Chua <34739863+Icyautumn@users.noreply.github.com>
2023-01-31 13:44:04 +0800

    Update loginServlet.java
    updated loginservlet

--- src/main/java/servlets/loginServlet.java
+++ src/main/java/servlets/loginServlet.java
@@ -1,15 +1,11 @@
 package servlets;
+
 import java.io.IOException;
 import org.mindrot.jbcrypt.*;
-
-import entities.Report;
-import models.ReportModel;
-
 import static javax.swing.JOptionPane.showMessageDialog;
 
 import java.io.PrintWriter;
 import java.util.Base64;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,77 +20,74 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/loginServlet")
 public class loginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public loginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public loginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-	
+
 	public static String encodeToBase64(String message) {
 		return Base64.getEncoder().encodeToString(message.getBytes());
 	}
-	
+
 	public static String decodeFromBase64(String encodedMessage) {
-		  byte[] decodedBytes = Base64.getDecoder().decode(encodedMessage);
-		  String decodedString = new String(decodedBytes);
-		  return decodedString;
-		}
+		byte[] decodedBytes = Base64.getDecoder().decode(encodedMessage);
+		String decodedString = new String(decodedBytes);
+		return decodedString;
+	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		try (PrintWriter out = response.getWriter()){
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try (PrintWriter out = response.getWriter()) {
 			String email = request.getParameter("username");
-			String password = request.getParameter("password");	
-			
+			String password = request.getParameter("password");
+
 			HttpSession session = request.getSession();
 //			 hash password
 //			 var hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
 //			BCrypt.checkpw(password, passwordcallfromdatabase);
-			
+
 			password = encodeToBase64(password);
 //			System.out.println(decodeFromBase64(password)+ " decoded");
-			
-			if(email.equals("inventory") && password.equals(encodeToBase64("password"))) {
-				session.setAttribute("getAlert",null);
+
+			if (email.equals("inventory") && password.equals(encodeToBase64("password"))) {
+				session.setAttribute("getAlert", null);
 				session.setAttribute("companyID", "002");
 
 				System.out.println("connected");
-				
-				//sets company ID to session attribute
+
+				// sets company ID to session attribute
 				System.out.println("company id: " + session.getAttribute("companyID"));
-				
-				ReportModel reportModel = new ReportModel();
 
-				List<Report> action = reportModel.findAll();
-
-				session.setAttribute("reportData", action);
-				request.getRequestDispatcher("/reports.jsp").forward(request, response);
-//				request.getRequestDispatcher("/ReportServlet.jsp").forward(request, response);
+				request.getRequestDispatcher("/ReportServlet.jsp").forward(request, response);
 //				response.sendRedirect("summary.jsp");
-				
-			}
-			else {
-				session.setAttribute("getAlert","Yes");
+
+			} else {
+				session.setAttribute("getAlert", "Yes");
 				session.setAttribute("CompanyID", "invalid");
-				
+
 				System.out.println(session.getAttribute("companyID") + " companyID");
 				System.out.println(session.getAttribute("getAlert") + " hello");
 				response.sendRedirect("login.jsp");
-				
+
 			}
 		}
 		doGet(request, response);
